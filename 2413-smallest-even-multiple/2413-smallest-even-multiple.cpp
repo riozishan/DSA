2a2af63d4a428bfb59e47af3f4e5ace05c9dbbd4
Rio Zishan <growingbird08@gmail.com>
2026-08-09 09:14:47 +0530

    Time: 0 ms (100%), Space: 8 MB (32.03%) - LeetHub

---
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int m = n;
+        for(n; n< (2*n); n++){
+            if((n%m==0)&&(n%2==0)){
+                m = n;
+                break;
+            }
+        }
+        return m;
+    }
+};
