e7274d094cfbb3bb0e2c3d076536706c8f8f553f
Rio Zishan <growingbird08@gmail.com>
2026-08-28 21:55:57 +0530

    Time: 2 ms (66.78%), Space: 18 MB (58.36%) - LeetHub

---
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        int count = 1;
+        int maxc = 1;
+        for(int i = 1; i< s.length(); i++){
+            if(((int)s[i]) == ((int)s[i-1])+1){
+                count++;
+            }
+            else{
+                count = 1;
+            }
+            maxc = max(maxc, count);
+        }
+        return maxc;
+    }
+};
