21b04a29da1b3a3c126872af88211ffc262d770e
Rio Zishan <growingbird08@gmail.com>
2026-08-28 21:56:22 +0530

    Time: 7 ms (32.32%), Space: 18 MB (86.76%) - LeetHub

--- 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
+++ 2414-length-of-the-longest-alphabetical-continuous-substring/2414-length-of-the-longest-alphabetical-continuous-substring.cpp
@@ -3,11 +3,10 @@ public:
     int longestContinuousSubstring(string s) {
         int count = 1;
         int maxc = 1;
-        for(int i = 1; i< s.length(); i++){
-            if(((int)s[i]) == ((int)s[i-1])+1){
+        for (int i = 1; i < s.length(); i++) {
+            if (((int)s[i]) == ((int)s[i - 1]) + 1) {
                 count++;
-            }
-            else{
+            } else {
                 count = 1;
             }
             maxc = max(maxc, count);
