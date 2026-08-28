7b82e123bdddbef3716dfa8bb1d012647f007b8e
Rio Zishan <growingbird08@gmail.com>
2026-08-28 11:51:32 +0530

    Time: 0 ms (100%), Space: 8.3 MB (40.1%) - LeetHub

---
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkZeroOnes(string s) {
+        int ocount = 1;
+        int zcount = 1;
+        int maxocount = 0;
+        int maxzcount = 0;
+        if(s.length()==1){
+            if(s[0]=='1'){
+                maxocount++;
+            }
+        }
+        for (int i = 1; i < s.length(); i++) {
+            if (s[i] == s[i - 1]) {
+                if (s[i] == '1') {
+                    ocount++;
+                } else {
+                    zcount++;
+                }
+            } 
+            else {
+                if (s[i] == '1') {
+                    ocount = 1;
+                } else {
+                    zcount = 1;
+                }
+            }
+            maxocount = max(maxocount, ocount);
+            maxzcount = max(maxzcount, zcount);
+        }
+        return (maxocount > maxzcount);
+    }
+};
