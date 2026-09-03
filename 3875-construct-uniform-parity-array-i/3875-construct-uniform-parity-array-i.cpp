e1b302644e270c9c49af882124962952b76948ea
Rio Zishan <growingbird08@gmail.com>
2026-09-03 14:04:20 +0530

    Time: 0 ms (100%), Space: 30.4 MB (36.41%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -2,7 +2,36 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
-        vector <int> nums2(n);
-        return true;
+        int oddcount = 0, evencount = 0;
+        for (int x : nums1) {
+            if (x % 2 == 0) {
+                evencount++;
+            } else {
+                oddcount++;
+            }
+        }
+        int allOdd = 1;
+        for (int x : nums1) {
+            if (x % 2 == 0) {
+                if (oddcount == 0) {
+                    allOdd = 0;
+                    break;
+                }
+            }
+        }
+        int allEven = 1;
+        for (int x : nums1) {
+            if (x % 2 == 1) {
+                if (oddcount < 2 && evencount == 0) {
+                    allEven = 1;
+                    break;
+                }
+                if (evencount == 0 && oddcount == 1) {
+                    allEven = 0;
+                    break;
+                }
+            }
+        }
+        return allOdd || allEven;
     }
 };
