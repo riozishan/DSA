7a314c47d140c53e4f89c6bc8836c696caacbfc7
Rio Zishan <growingbird08@gmail.com>
2026-09-10 21:25:13 +0530

    Time: 9 ms (38.11%), Space: 28.5 MB (84.96%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.cpp
@@ -3,8 +3,8 @@ public:
     int repeatedNTimes(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int ele;
-        for (int i = 1; i < nums.size(); i++) {
-            if (nums[i] == nums[i - 1]) {
+        for(int i = 1; i<nums.size(); i++){
+            if(nums[i]==nums[i-1]){
                 ele = nums[i];
                 break;
             }
